669308e53c06f06407107362858277668a3cb9cb
Newton Muhindi <developermuhindi@gmail.com>
2025-05-26 19:06:45 +0300

    complete  lab

--- lib/department.py
+++ lib/department.py
@@ -10,3 +10,61 @@ class Department:
 
     def __repr__(self):
         return f"<Department {self.id}: {self.name}, {self.location}>"
+
+    @classmethod
+    def create_table(cls):
+        """Create a new table to persist the attributes of Department instances."""
+        sql = """
+            CREATE TABLE IF NOT EXISTS departments (
+                id INTEGER PRIMARY KEY,
+                name TEXT,
+                location TEXT)
+        """
+        CURSOR.execute(sql)
+        CONN.commit()
+        
+    @classmethod
+    def drop_table(cls):
+        """Drop the table that persists Department instances."""
+        sql = """
+            DROP TABLE IF EXISTS departments
+        """
+        CURSOR.execute(sql)
+        CONN.commit()
+        
+    def save(self):
+        sql = """
+            INSERT INTO departments (name, location)
+            VALUES (?, ?)
+        """
+        
+        CURSOR.execute(sql, (self.name, self.location))
+        CONN.commit()
+        
+        self.id = CURSOR.lastrowid
+        
+    @classmethod
+    def create(cls, name, location):
+        """Initialize a new Department instance and save it to the database."""
+        department = cls(name, location)
+        department.save()
+        return department
+        
+    def update(self):
+        """Update the department's record in the database."""
+        sql = """
+            UPDATE departments
+            SET name = ?, location = ?
+            WHERE id = ?
+        """
+        CURSOR.execute(sql, (self.name, self.location, self.id))
+        CONN.commit()
+        
+    def delete(self):
+        """Delete the department's record from the database."""
+        sql = """
+            DELETE FROM departments
+            WHERE id = ?
+        """
+        CURSOR.execute(sql, (self.id,))
+        CONN.commit()
